13a2939fe1713b20031ac033061fa8c6a3a7c322
FourZd <shogo.ryaskov@gmail.com>
2023-06-03 19:09:32 +0300

    complete split up

--- parser/__main__.py
+++ parser/__main__.py
@@ -4,7 +4,7 @@ import math
 import asyncio
 import random
 from datetime import datetime
-import re 
+import re
 
 from dbo import (
     get_cursor,
@@ -12,12 +12,13 @@ from dbo import (
     get_buff_credentials,
     get_steam_credentials,
     insert_items,
-    insert_cookies
+    insert_cookies,
 )
 from run_selenium import login, get_firefox_profile
 
 
 async def run_period(cursor, conn, bot, period, game):
+    print(f'Period {period} has started at {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
     for i in range(period[0], period[1] + 1):
         page = await get_page(
             page_num=i,
@@ -27,7 +28,6 @@ async def run_period(cursor, conn, bot, period, game):
             proxy_username=bot["proxy_user"],
             proxy_password=bot["proxy_password"],
         )
-
         try:
             item_list = []
             items = page["data"]["items"]
@@ -42,20 +42,25 @@ async def run_period(cursor, conn, bot, period, game):
                     "steam_link": item["steam_market_url"],
                 }
                 item_list.append(skin)
-            print(len(item_list))
+            print(f'Item list contains {len(item_list)} items')
             await insert_items(cursor, conn, item_list)
         except KeyError:
-            print(page)
-            print(bot)
+            print('WARNING')
+            print('KEY EXCEPTION ON PAGE', i)
+            print('USING BOT', bot)
 
 
 async def get_page(
     page_num, cookies, game, proxy, proxy_username, proxy_password, rarity=None
 ):
     url = (
+        
+    )
+    url = (
         f"https://buff.163.com/api/market/goods?game={game}&page_num={page_num}&use_suggestion=0&_={round(time.time() * 1000)}"
         + (f"&rarity={rarity}" if rarity else "")
     )
+    print(f'Requesting page {url}')
     response = requests.get(
         url,
         cookies=cookies,
@@ -63,15 +68,16 @@ async def get_page(
             "http": f"http://{proxy_username}:{proxy_password}@{proxy}",
             "https": f"https://{proxy_username}:{proxy_password}@{proxy}",
         },
-        timeout=5,
+        timeout=50,
     )
+    print('Status code is', response.status_code)
+    print('Sleeping... Zzz....')
     await asyncio.sleep(random.uniform(2.00, 5.00))
     print("Sleep done", page_num)
     return response.json()
 
 
 async def run_parser(cursor, conn, bots):
-
     # Get amount of bots to split pages between them
     bots_amount = len(bots)
 
@@ -80,12 +86,12 @@ async def run_parser(cursor, conn, bots):
 
     # Get total page number
     total_pages = await get_page(
-        page_num=1, 
-        cookies=first_bot['cookie'],
-        game='csgo',
-        proxy=first_bot['proxy'],
-        proxy_username=first_bot['proxy_user'],
-        proxy_password=first_bot['proxy_password'],
+        page_num=1,
+        cookies=first_bot["cookie"],
+        game="csgo",
+        proxy=first_bot["proxy"],
+        proxy_username=first_bot["proxy_user"],
+        proxy_password=first_bot["proxy_password"],
     )
     try:
         total_pages = total_pages["data"]["total_page"]
@@ -94,19 +100,24 @@ async def run_parser(cursor, conn, bots):
 
     # Split pages between bots
     period_size = math.ceil(total_pages / bots_amount)
-    print('Period size is', period_size)
+    print("Period size is", period_size)
 
     tasks = []
     for i in range(bots_amount):
         # [(1, 15), (16, 30), (31, 45), (46, 60)]
         if i == 0:
-            period = (i+1, period_size)
+            period = (i + 1, period_size)
         else:
-            period = (i*period_size+1, (i+1)*period_size)
-        tasks.append(asyncio.create_task(run_period(bots[i], period, game)))
+            period = (i * period_size + 1, (i + 1) * period_size)
+        tasks.append(
+            asyncio.create_task(
+                run_period(
+                    bot=bots[i], period=period, game="csgo", conn=conn, cursor=cursor
+                )
+            )
+        )
+    print('Starting parser...')
     await asyncio.gather(*tasks)
-    await run_period(cursor, conn)
-
 
 async def get_cookies(cursor, bots, conn):
     # Get bots credentials for authentication with proxy
@@ -115,24 +126,40 @@ async def get_cookies(cursor, bots, conn):
     for account in steam_creds:
         proxy = [
             {
-                'proxy_host': bot['proxy'],
-                'proxy_username': bot['proxy_user'],
-                'proxy_password': bot['proxy_password']
-            } for bot in bots if bot['id'] == account['id']
-        ][0]
-        
-        profile = await get_firefox_profile(proxy['proxy_host'], proxy['proxy_username'], proxy['proxy_password'])
-        cookies = await login(account['steam_login'], account['steam_password'], account['email'], account['email_password'], profile)
-        await insert_cookies(cursor, account['id'], cookies, conn)
+                "proxy_host": bot["proxy"],
+                "proxy_username": bot["proxy_user"],
+                "proxy_password": bot["proxy_password"],
+            }
+            for bot in bots
+            if bot["id"] == account["id"]
+        ]
+        print(proxy)
+        if len(proxy) > 0:
+            print(proxy)
+            proxy = proxy[0]
+
+            profile = await get_firefox_profile(
+                proxy["proxy_host"], proxy["proxy_username"], proxy["proxy_password"]
+            )
+            cookies = await login(
+                account["steam_login"],
+                account["steam_password"],
+                account["email"],
+                account["email_password"],
+                profile,
+            )
+            await insert_cookies(cursor, account["id"], cookies, conn)
+
 
 async def run():
     conn = get_connection()
     cursor = get_cursor(conn)
 
     # Get bots credentials for authentication with proxy
-    bots = get_buff_credentials(cursor)
-
+    bots = get_buff_credentials(cursor, cookie=False)
     await get_cookies(cursor, bots, conn)
+
+    bots = get_buff_credentials(cursor, cookie=True)
     await run_parser(cursor, conn, bots)
 
 
